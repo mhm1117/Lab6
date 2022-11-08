b162d60fcb1a792f3d1838862b8c3fbc1e72b336
mhm1117 <83431136+mhm1117@users.noreply.github.com>
2022-11-07 20:11:29 -0800

    Update main.js

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -24,7 +24,10 @@ function getRecipesFromStorage() {
   // A9. TODO - Complete the functionality as described in this function
   //           header. It is possible in only a single line, but should
   //           be no more than a few lines.
-  return JSON.parse(localStorage.getItem("recipes"));
+  let recipes = localStorage.getItem("recipes");
+  if (recipes == null)
+    return [];
+  return JSON.parse(recipes);
 }
 
 /**
